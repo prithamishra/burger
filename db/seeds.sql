696ff43b0c8578830306a4722936e117dc050936
Pritha Mishra <prithamishra1@gmail.com>
2018-05-13 15:47:59 -0600

    push to git

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Smash Chicken Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swish mushroom Bagel Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Funny Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
